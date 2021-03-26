18756563ae85034d68ae3690aefc5e60e112d556
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-26 00:32:37 -0300

    Entrega de la actividad 4, a pedido del docente.

---
+++ Actividad_04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        //Permita el ingreso de frases, finalizando cuando el usuario ingresa “fin”.
+        {
+            string finalizar;
+
+            do
+            {
+                Console.WriteLine("Ingrese una frase/s: ");
+              
+                finalizar = Console.ReadLine();
+                Console.WriteLine(" \r ");
+
+                Console.WriteLine("Para finalizar el programa, debe ingresar la palabra 'fin' ");
+            } while (finalizar != "fin");
+            
+    }
+    }
+}
